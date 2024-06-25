49157564808aed7db690fd888360c16d6d2a1072
JustynLim <89184180+JustynLim@users.noreply.github.com>
2024-06-25 14:56:29 +0800

    misc changes

--- clinic.py
+++ clinic.py
@@ -1,271 +1,126 @@
-from ui_clinic import Ui_MainWindow
-from PySide6.QtWidgets import *
-from PySide6.QtCore import Slot
-import sqlite3
-import os
-import random
-import string
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from PyQt5 import QtCore, QtGui,QtWidgets
+from UI.Login_UI import *; from UI.Register_UI import *
+#from UI.Login_UI import Login_Form; from UI.Register_UI import Register_Form
+from UI.Login_Resources import *; from UI.Register_Resources import *
+from utilities import login, register, login_display_status, register_display_status
+import sys
 
-class MySideBar(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+
+#  Global variables
+#path_db_file    = "db/database.db"
+form_type       = 1
+logged_in_email = None
+
+#  Allows window to be dragged around the screen
+class DraggableWidget(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._mousePressed = False
+        self._mousePos = None
+        self._startGeometry = None
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self._mousePressed = True
+            self._mousePos = event.globalPos()
+            self._startGeometry = self.geometry()
+
+    def mouseMoveEvent(self, event):
+        if self._mousePressed:
+            delta = event.globalPos() - self._mousePos
+            self.move(self._startGeometry.topLeft() + delta)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self._mousePressed = False
+
+
+class MainController(DraggableWidget):
+    login_finished = QtCore.pyqtSignal(bool) # Signal to indicate login finished
+    registration_finished = QtCore.pyqtSignal(bool) # added this
+    
+    def __init__(self, screen):
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("Call A Doctor")
-
-        # Show the home page first
-        self.switch_to_homePage()
-
-        self.widget_2.setHidden(True)
-
-        #connect the buttons to repective pages
-        self.home_1.clicked.connect(self.switch_to_homePage)
-        self.home_2.clicked.connect(self.switch_to_homePage)
-
-        self.adddoctor_1.clicked.connect(self.switch_to_addDoctorPage)
-        self.adddoctor_2.clicked.connect(self.switch_to_addDoctorPage)
-        self.shortcut_add.clicked.connect(self.switch_to_addDoctorPage)
-
-        self.viewdoctor_1.clicked.connect(self.switch_to_viewDoctorPage)
-        self.view_doctor_2.clicked.connect(self.switch_to_viewDoctorPage)
-        self.shortcut_view.clicked.connect(self.switch_to_viewDoctorPage)
-
-        self.addschedule_1.clicked.connect(self.switch_to_addSchedulePage)
-        self.addschedule_2.clicked.connect(self.switch_to_addSchedulePage)
-        self.shortcut_schedule.clicked.connect(self.switch_to_addSchedulePage)
-
-        self.home_1.clicked.connect(self.switch_to_homePage)
-        self.home_2.clicked.connect(self.switch_to_homePage)
-
-        self.confirm_button.clicked.connect(self.add_doctor) 
-
-        # Connect the delete button to its function
-        self.delete_button.clicked.connect(self.delete_selected_doctor)
-
-        # Load data into doctor_table
-        self.load_doctors_data()
-
-    def switch_to_homePage(self):
-        self.stackedWidget.setCurrentIndex(0)
-
-    def switch_to_addDoctorPage(self):
-        self.stackedWidget.setCurrentIndex(1)
-
-    def switch_to_viewDoctorPage(self):
-        self.stackedWidget.setCurrentIndex(2)
-        self.load_doctors_data()  # Reload data whenever switching to the view doctor page
-
-    def switch_to_addSchedulePage(self):
-        self.stackedWidget.setCurrentIndex(3)
-
-    def load_doctors_data(self):
-        # Define the path to the database file
-        db_path = os.path.join(os.path.dirname(__file__), 'db')
-        path_db_file = os.path.join(db_path, "database.db")
-        
-        # Debug: Print the database path
-        print(f"Database path: {path_db_file}")
-        
-        # Check if the database file exists
-        if not os.path.exists(path_db_file):
-            print(f"Database file does not exist at path: {path_db_file}")
-            return
-
-        # Connect to SQLite3 database
-        connection = sqlite3.connect(path_db_file)
-        cursor = connection.cursor()
-
-        # Execute query to retrieve data
-        cursor.execute("SELECT DOCTOR_EMAIL, DOCTOR_NAME, DOCTOR_PHONENO, DOCTOR_ADDRESS FROM DOCTORS")
-        rows = cursor.fetchall()
-
-        # Set table row and column count
-        self.doctor_table.setRowCount(len(rows))
-        self.doctor_table.setColumnCount(4)
-
-        # Set table headers
-        headers = ["Doctor's Email", "Doctor's Name", "Doctor's Phone Number", "Doctor's Address"]
-        self.doctor_table.setHorizontalHeaderLabels(headers)
-
-        # Populate table with data
-        for row_idx, row_data in enumerate(rows):
-            for col_idx, col_data in enumerate(row_data):
-                self.doctor_table.setItem(row_idx, col_idx, QTableWidgetItem(col_data))
-
-        # Close the database connection
-        connection.close()
-
-    @Slot()
-    def delete_selected_doctor(self):
-        # Get the selected row
-        selected_row = self.doctor_table.currentRow()
-
-        if selected_row >= 0:  # Ensure a row is selected
-            # Get the doctor email from the selected row (assuming it's in the first column)
-            doctor_email = self.doctor_table.item(selected_row, 0).text()
-
-            # Define the path to the database file
-            db_path = os.path.join(os.path.dirname(__file__), 'db')
-            path_db_file = os.path.join(db_path, "database.db")
-
-            # Connect to SQLite3 database
-            connection = sqlite3.connect(path_db_file)
-            cursor = connection.cursor()
-
-            # Delete the doctor from the DOCTORS table
-            cursor.execute("DELETE FROM DOCTORS WHERE DOCTOR_EMAIL = ?", (doctor_email,))
-            # Delete the corresponding entry from the KEY_LIST table
-            cursor.execute("DELETE FROM KEY_LIST WHERE ASSIGNED_TO_EMAIL = ?", (doctor_email,))
-            connection.commit()
-
-            # Close the database connection
-            connection.close()
-
-            # Remove the row from the table
-            self.doctor_table.removeRow(selected_row)
-        else:
-            self.show_no_selection_message()
-
-    def show_no_selection_message(self):
-        msg_box = QMessageBox()
-        msg_box.setIcon(QMessageBox.Icon.Warning)
-        msg_box.setWindowTitle("No Selection")
-        msg_box.setText("No row selected. Please select a row to delete.")
-        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-        msg_box.exec()
-
-    def generate_activation_code(self):
-        return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-
-    def send_approved_email(self, email, activation_code):
-        smtp_server = 'smtp.gmail.com'
-        smtp_port = 587  # Gmail's appropriate port
-        sender_email = os.environ.get('PYTHON_BACKEND_SENDER_EMAIL_CREDENTIALS')
-        sender_password = os.environ.get('PYTHON_BACKEND_SENDER_PASSWORD_CREDENTIALS')
-
-        if not sender_email or not sender_password:
-            print("Error: Email credentials are not set in the environment variables.")
-            return
-
-        try:
-            # Connect to the SMTP server
-            server = smtplib.SMTP(smtp_server, smtp_port)
-            server.starttls()
-            server.login(sender_email, sender_password)
-
-            body = f"""<html>
-                        <body>
-                            <p>This is a confirmation email to acknowledge that your application has been approved. Please find your activation code attached below</p>
-                            <p>Activation Code: {activation_code}</p>
-                            <p></p>
-                            <p></p>
-                            <p>Head on over to our registration page to create an account for your profile.</p>
-                            <p>The activation code is only valid for a single use. Please do not share it to anybody else</p>
-                            <p></p>
-                            <p>Regards,</p>
-                            <p>Administration of Call a Doctor</p>
-                        </body>
-                        </html>"""
-                
-            msg = MIMEMultipart()
-            msg['From'] = sender_email
-            msg['To'] = email
-            msg['Subject'] = "Application Status"
-            msg.attach(MIMEText(body, 'html'))
-
-            # Send the email
-            text = msg.as_string()
-            server.sendmail(sender_email, email, text)
-
-            # Close the connection
-            server.quit()
-
-            print(f"Approved email has been sent to user's email address: {email}")
-        except Exception as e:
-            print("Email error:", e)
-            print("Email sending failed. Please check your email settings.")
-
-    @Slot()
-    def add_doctor(self):
-        email = self.doc_mail_edit.text()
-        name = self.doc_name_edit.text()
-        phone = self.doc_phone_edit.text()
-        address = self.doc_add_edit.text()
-
-        if email and name and phone and address:
-            activation_code = self.generate_activation_code()
             
-            # Define the path to the database file
-            db_path = os.path.join(os.path.dirname(__file__), 'db')
-            path_db_file = os.path.join(db_path, "database.db")
+        # Initialise UI widgets
+        self.login_widget = Login_Form(self)
+        self.register_widget = Register_Form(self)
 
-            # Connect to SQLite3 database
-            connection = sqlite3.connect(path_db_file)
-            cursor = connection.cursor()
+        # Creates a central widget to hold all forms and a layout manager
+        self.stacked_layout = QtWidgets.QStackedLayout(self)
+        self.stacked_layout.addWidget(self.login_widget)
+        self.stacked_layout.addWidget(self.register_widget)
 
-            # Insert the new doctor and activation code into the database
-            placeholder_password_hash = ""  # Use an empty string as a placeholder
-            cursor.execute("""
-                INSERT INTO DOCTORS (DOCTOR_EMAIL, DOCTOR_NAME, DOCTOR_PHONENO, DOCTOR_ADDRESS, PASSWORD_HASH, ACTIVATION_CODE) 
-                VALUES (?, ?, ?, ?, ?, ?)""", (email, name, phone, address, placeholder_password_hash, activation_code))
-            cursor.execute("""
-                INSERT INTO KEY_LIST (ACTIVATION_CODE, ASSIGNED_TO_EMAIL, ACTIVATION_STATUS) 
-                VALUES (?, ?, 'Not Activated')""", (activation_code, email))
-            connection.commit()
+        # Set the central widget of the main window
+        self.setLayout(self.stacked_layout)
 
-            # Close the database connection
-            connection.close()
+        # Connect buttons for page switching
+        self.login_widget.pushButton.clicked.connect(lambda: login(self, self.login_widget.lineEdit.text(), self.login_widget.lineEdit_2.text()))
+        self.login_widget.toolButton.clicked.connect(self.show_register_form)
+        self.register_widget.toolButton_5.clicked.connect(self.show_login_form)
+        # self.register_widget.pushButton_2.clicked.connect(lambda: register(self.register_widget, self.register_widget.lineEdit_5.text(), self.register_widget.lineEdit_6.text(), self.register_widget.lineEdit_7.text(), self.register_widget.lineEdit_8.text()))
+        ###self.register_widget.pushButton_2.clicked.connect(lambda: register(self.register_widget.lineEdit_5.text(), self.register_widget.lineEdit_6.text(), self.register_widget.lineEdit_8.text()))
 
-            # Send the activation email
-            self.send_approved_email(email, activation_code)
+        self.login_finished.connect(self.display_login_status)
+        self.registration_finished.connect(self.display_registration_status) # added this
 
-            # Clear the input fields
-            self.doc_mail_edit.clear()
-            self.doc_name_edit.clear()
-            self.doc_phone_edit.clear()
-            self.doc_add_edit.clear()
+        # Initialise program with login form
+        self.show_login_form()
 
-            # Show confirmation pop-up
-            QMessageBox.information(self, "Doctor Added", "Doctor is added and activation email sent.")
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
-            print("Doctor added and activation email sent.")
+        # screen = QtWidgets.QApplication.primaryScreen()
+        dpi = screen.physicalDotsPerInch()
+        scaling_factor = dpi / 96.0     # Standard DPI is 96
+        self.resize(int(750 * scaling_factor), int(540 * scaling_factor))
+        
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+
+
+    def show_register_form(self):
+        # QtCore.QTimer.singleShot(500, lambda: self.stacked_layout.setCurrentWidget(self.register_widget))
+        self.stacked_layout.setCurrentWidget(self.register_widget)
+        self.register_widget.lineEdit_5.setFocus()
+        # self.register_widget.lineEdit_5.activateWindow()
+
+
+    def show_login_form(self):
+        # QtCore.QTimer.singleShot(500, lambda: self.stacked_layout.setCurrentWidget(self.login_widget))
+        self.stacked_layout.setCurrentWidget(self.login_widget)
+
+    def handle_login(self):
+        email = self.login_widget.lineEdit.text().lower()
+        password = self.login_widget.lineEdit_2.text()
+        login_result = login(self, email, password)  # Update utilities.py accordingly
+        self.login_finished.emit(login_result)  # Emit signal after login attempt
+
+    def handle_register_XX(self):
+        email = self.register_widget.lineEdit_5.text().lower()
+        password = self.register_widget.lineEdit_6.text()
+        confirm_password = self.register_widget.lineEdit_7.text()
+        activation_code = self.register_widget.lineEdit_8.text()
+        register_result = register(self, email, password, confirm_password, activation_code)
+        self.registration_finished.emit(register_result)
+
+    def display_login_status(self, success):
+        if success:
+            login_display_status(self, 1)
         else:
-            self.show_incomplete_form_message()
+            login_display_status(self, 0)
 
-    def show_incomplete_form_message(self):
-        msg_box = QMessageBox()
-        msg_box.setIcon(QMessageBox.Icon.Warning)
-        msg_box.setWindowTitle("Incomplete Form")
-        msg_box.setText("Please fill in all fields.")
-        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-        msg_box.exec()
-
-def activate_doctor(email, password_hash):
-    # Define the path to the database file
-    db_path = os.path.join(os.path.dirname(__file__), 'db')
-    path_db_file = os.path.join(db_path, "database.db")
-
-    # Connect to SQLite3 database
-    connection = sqlite3.connect(path_db_file)
-    cursor = connection.cursor()
-
-    # Update the doctor's password hash and set the status to "activated"
-    cursor.execute("""
-        UPDATE DOCTORS SET PASSWORD_HASH = ? WHERE DOCTOR_EMAIL = ?""", (password_hash, email))
-    cursor.execute("""
-        UPDATE KEY_LIST SET ACTIVATION_STATUS = 'Activated' WHERE ASSIGNED_TO_EMAIL = ?""", (email,))
-    connection.commit()
-
-    # Close the database connection
-    connection.close()
-
-    print(f"Doctor {email} activated.")
-
+    def display_registration_status(self, status):
+        register_display_status(self, status)
+        
 if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
-    window = MySideBar()
-    window.show()
-    sys.exit(app.exec())
+    import ctypes
+    ctypes.windll.shcore.SetProcessDpiAwareness(2)
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+
+    app = QtWidgets.QApplication(sys.argv)
+    screen = app.primaryScreen()
+    controller = MainController(screen)
+    controller.register_widget.registration_finished.connect(controller.display_registration_status)
+
+    controller.show()
+    sys.exit(app.exec_())
